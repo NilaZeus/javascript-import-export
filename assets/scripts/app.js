79426dab124f76916811c0870ba1758793ab0d53
NilaZeus <106056349+NilaZeus@users.noreply.github.com>
2024-12-12 11:17:37 +0530

    Initial commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,8 @@
+import { abc } from "./util.js";
+//import abcs from "./util.js";
+//import { abc, mns } from "./util.js";
+//import { abc, mns as bbb} from "./util.js";
+//import * as util from "./util.js";
+
+console.log(abc);
+//console.log(util.abc);
---
+++ assets/scripts/util.js
@@ -0,0 +1,2 @@
+export let abc = "Nilanjan";
+//export default "ansgs";
